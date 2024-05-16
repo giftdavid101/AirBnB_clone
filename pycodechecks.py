2a0fe7443e07d0dca916d99c95c78e5454d4e05f
giftdavid101 <giftdavid101@gmail.com>
2024-05-16 14:50:04 +0000

    pycodechecks

--- pycodechecks.py
+++ pycodechecks.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 
+
 def checksCodeStyle():
-     """
+    """
     This function checks for pycodestyle.
-    
     Parameters:
         void ()
-    
     Returns:
         str
     """
     print("Checks for pycodestyle")
     return "Check for string with pycodestyle"
-
